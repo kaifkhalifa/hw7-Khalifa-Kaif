ffa5e379e15bd03ff580fb816a48c48130141cca
Kaif Khalifa <81500937+kaifkhalifa@users.noreply.github.com>
2024-10-23 14:47:20 -0400

    fixing keyhandler

--- hw7.rkt
+++ hw7.rkt
@@ -13,196 +13,202 @@
 (define TEXTBOX-WIDTH 512)
 (define TEXTBOX-HEIGHT 40)
 (define INITIAL-SPACE 4)
+(define CURSOR (rectangle CURSOR-WIDTH FONT-SIZE "solid" "black"))
+(define TEXTBOX (rectangle TEXTBOX-WIDTH TEXTBOX-HEIGHT "outline" "black"))
 
-;; A TextBox is a (TextBox [pre: list] [post: list])
+;; A TextBox is a (create-TextBox [right: list] [left: list])
 ;; where
-;; pre: represents the list of characters to the left of the cursor
-;; post: represents the list of characters to the right of the cursor
-(struct TextBox [pre post] #:transparent) 
-
-
-;; create-TextBox : String String -> TextBox?
-;; Takes two strings (pre and post) and creates a TextBox instance.
+;; pre: represents the list of characters before the cursor
+;; post: represents the list of characters after the cursor
+(struct TextBox [pre post] #:transparent)
 (define/contract (create-TextBox pre post)
   (-> string? string? TextBox?)
-  (TextBox (string->list pre) (string->list post)))
+  (TextBox pre post))
 
-;; A WorldState is a TextBox
-;; interp: represents the text box the user types characters into
-(define (WorldState? w)
-  (TextBox? w))
+;; string-to-text: string -> image
+;; takes a list and converts it to text to go in the textbox
+(define/contract (string-to-text str)
+  (-> string? image?)
+  (text str FONT-SIZE FONT-COLOR))
+;; example:
+(check-equal? (string-to-text "hi") (text "hi" FONT-SIZE FONT-COLOR))
 
-;; render : TextBox -> Image
-;; Takes an instance of TextBox, and renders the text and cursor into a box.
-(define (render textbox)
-  (let* ([pre-str (list->string (TextBox-pre textbox))]  ;; Do not reverse the pre list
-         [post-str (list->string (TextBox-post textbox))]
-         [pre-text (text pre-str FONT-SIZE FONT-COLOR)]
-         [post-text (text post-str FONT-SIZE FONT-COLOR)]
-         [cursor (rectangle CURSOR-WIDTH FONT-SIZE "solid" "black")]
-         [text-image (beside pre-text cursor post-text)])
-    (place-image text-image 
-                 (+ INITIAL-SPACE (/ (image-width text-image) 2)) 
-                 (/ TEXTBOX-HEIGHT 2)
-                 (rectangle TEXTBOX-WIDTH TEXTBOX-HEIGHT "outline" "black"))))
+;; create-text-img: TextBox -> TextBox
+;; creates the image that enpsulates the text and cursor which goes inside of the textbox
+(define/contract (create-text-img tb)
+  (-> TextBox? image?)
+  (match-define (TextBox pre post) tb)
+  (beside (string-to-text pre) CURSOR (string-to-text post)))
+;; example:
+(check-equal? (create-text-img (create-TextBox "hel" "lo"))
+              (beside (string-to-text "hel") CURSOR (string-to-text "lo")))
+
+;; text-center-x: image -> number
+;; calculates the x-coordinate for the center of an image on the textbox
+(define/contract (text-center-x img)
+  (-> image? number?)
+  (+ INITIAL-SPACE (/ (image-width img) 2)))
+;; example:
+(check-equal? (text-center-x (create-text-img (create-TextBox "h" "i")))
+              (+ INITIAL-SPACE (/ (image-width (create-text-img (create-TextBox "h" "i"))) 2)))
+
+;; render: WorldState -> image
+;; Renders the text and cursor in a textbox
+(define/contract (render tb)
+  (-> TextBox? image?)
+  (place-image (create-text-img tb) (text-center-x (create-text-img tb)) (/ TEXTBOX-HEIGHT 2) TEXTBOX))
+;; example:
+(check-equal? (render (create-TextBox "hel" "lo"))
+              (place-image (create-text-img (create-TextBox "hel" "lo"))
+                           (text-center-x (create-text-img (create-TextBox "hel" "lo")))
+                           (/ TEXTBOX-HEIGHT 2) TEXTBOX))
+
+;; last-index: string -> integer
+;; helper function to get the last index of a string 
+(define (last-index str)
+  (max 0 (- (string-length str) 1)))
+
+;; first-index: string -> string
+;; helper function to get the first index of a string 
+(define (first-index str)
+  (min 1 (string-length str)))
+
+;; textbox-delete
+;; removes the character right after the cursor
+(define/contract (textbox-delete tb)
+  (-> TextBox? TextBox?)
+  (match-define (TextBox pre post) tb)
+  (create-TextBox pre (substring post (first-index post))))
+;; example
+(check-equal? (textbox-delete (create-TextBox "hel" "lo")) (create-TextBox "hel" "o"))
 
 
-
-;; Example for render
-(define textbox-example (create-TextBox "Hello" "World"))
-
-(check-equal? (render textbox-example)
-              (place-image
-               (beside (text "Hello" FONT-SIZE FONT-COLOR) 
-                       (rectangle CURSOR-WIDTH FONT-SIZE "solid" "black")
-                       (text "World" FONT-SIZE FONT-COLOR))
-               (+ INITIAL-SPACE (/ (+ (image-width (text "Hello" FONT-SIZE FONT-COLOR))
-                                      CURSOR-WIDTH
-                                      (image-width (text "World" FONT-SIZE FONT-COLOR))) 2))
-               (/ TEXTBOX-HEIGHT 2)
-               (rectangle TEXTBOX-WIDTH TEXTBOX-HEIGHT "outline" "black")))
+;; textbox-backspace: TextBox -> TextBox
+;; removes the character right before the cursor
+(define/contract (textbox-backspace tb)
+  (-> TextBox? TextBox?)
+  (match-define (TextBox pre post) tb)
+  (create-TextBox (substring pre 0 (last-index pre)) post))
+;; example
+(check-equal? (textbox-backspace (create-TextBox "hel" "lo")) (create-TextBox "he" "lo"))
 
 
+;; textbox-insert: TextBox, string -> TextBox
+;; inserts string where the cursor is and moves the cursor to the right of that character
+(define/contract (textbox-insert tb str)
+  (-> TextBox? string? TextBox?)
+  (match-define (TextBox pre post) tb)
+  (create-TextBox (string-append pre str) post))
+;; example
+(check-equal? (textbox-insert (create-TextBox "h" "llo") "e") (create-TextBox "he" "llo"))
 
-;; remove-char : list -> list
-;; Removes the first character from the given list.
-(define (remove-char lst)
-  (if (empty? lst)
-      lst
-      (rest lst)))
+
+;; textbox-left: TextBox -> TextBox
+;; moves the cursor one character to the left
+(define/contract (textbox-left tb)
+  (-> TextBox? TextBox?)
+  (match-define (TextBox pre post) tb)
+  (create-TextBox (substring pre 0 (last-index pre)) 
+                      (string-append (substring pre (last-index pre)) post)))
+;; example
+(check-equal? (textbox-left (create-TextBox "hel" "lo")) (create-TextBox "he" "llo"))
+
+
+;; textbox-right: TextBox -> TextBox
+;; moves the cursor one character to the right
+(define/contract (textbox-right tb)
+  (-> TextBox? TextBox?)
+  (match-define (TextBox pre post) tb)
+  (create-TextBox (string-append pre (substring post 0 (first-index post)))
+                  (substring post (first-index post))))
+;; example
+(check-equal? (textbox-right (create-TextBox "hel" "lo")) (create-TextBox "hell" "o"))
+
+
+;; remove-char : TextBox -> TextBox
+;; Removes the first character in the `post` (right) part of the TextBox, right after the cursor.
+(define/contract (remove-char tb)
+  (-> TextBox? TextBox?)
+  (match-define (TextBox pre post) tb)
+  (create-TextBox pre (substring post (first-index post))))
 
 ;; Example
-(check-equal? 
-  (remove-char '(#\a #\b #\c))
-  '(#\b #\c))
+(check-equal? (remove-char (create-TextBox "hel" "lo")) (create-TextBox "hel" "o"))
 
-
-;; shift-char : list * list -> list * list
-;; Moves the first character from 'from-list' to the front of 'to-list'.
-(define (shift-char from-list to-list)
-  (if (empty? from-list)
-      (values from-list to-list)
-      (values (rest from-list) (cons (first from-list) to-list))))
+;; shift-char : TextBox -> TextBox
+;; Moves the first character from `post` to `pre`.
+(define/contract (shift-char tb)
+  (-> TextBox? TextBox?)
+  (match-define (TextBox pre post) tb)
+  (if (empty? post)
+      tb  ;; No characters to shift
+      (create-TextBox (string-append pre (substring post 0 1))  ;; Move first char from post to pre
+                      (substring post 1))))  ;; Remove first char from post
 
 ;; Example
-(define-values (new-from new-to)
-  (shift-char '(#\a #\b #\c) '(#\x #\y)))
-(check-equal? new-from '(#\b #\c))
-(check-equal? new-to '(#\a #\x #\y))
-
-
-;; textbox-left : TextBox -> TextBox
-;; Moves the cursor one character to the left.
-(define (textbox-left tb)
-  (define-values (new-pre new-post)
-    (shift-char (TextBox-pre tb) (TextBox-post tb)))
-  (if (equal? (TextBox-pre tb) new-pre)
-      tb
-      (TextBox new-pre new-post)))
-
-
-;; Example for textbox-left
-(define empty-left-textbox (create-TextBox "World" ""))
-
-(check-equal? 
-  (textbox-left empty-left-textbox)
-  (create-TextBox "orld" "W"))
-
-
-;; textbox-right : TextBox -> TextBox
-;; Moves the cursor one character to the right.
-(define (textbox-right tb)
-  (define-values (new-post new-pre)  ;; Reverse the lists correctly
-    (shift-char (TextBox-post tb) (TextBox-pre tb)))  ;; Move from 'post' to 'pre'
-  (if (equal? (TextBox-post tb) new-post)
-      tb 
-      (TextBox new-pre new-post)))  ;; Correctly swap the lists
-
-
-
-;; Example for textbox-right
-(define empty-right-textbox (create-TextBox "" "Hello"))
-
-(check-equal? 
-  (textbox-right empty-right-textbox)
-  (create-TextBox "H" "ello"))
-
-
-
-
-;; textbox-backspace : TextBox -> TextBox
-;; Removes the character to the left of the cursor.
-(define (textbox-backspace tb)
-  (TextBox (remove-char (TextBox-pre tb)) (TextBox-post tb)))
-
-
-;; Example for textbox-backspace
-(define empty-left-textbox2 (create-TextBox "World" ""))
-
-(check-equal? 
-  (textbox-backspace empty-left-textbox2)
-  (create-TextBox "orld" ""))
-
-
-;; textbox-delete : TextBox -> TextBox
-;; Removes the character to the right of the cursor.
-(define (textbox-delete tb)
-  (if (empty? (TextBox-post tb))
-      tb
-      (TextBox (TextBox-pre tb) (rest (TextBox-post tb)))))
-
-
-;; Example for textbox-delete
-(define example-textbox (create-TextBox "World" "Hello"))
-
-(check-equal? 
-  (textbox-delete example-textbox)
-  (create-TextBox "World" "ello"))
-
-
-;; textbox-insert : TextBox String -> TextBox
-;; Inserts a single character at the cursor and moves the cursor right.
-(define (textbox-insert tb char)
-  (if (not (= (string-length char) 1))
-      tb  ; Ignore if not a single character
-      (TextBox (cons (string-ref char 0) (TextBox-pre tb)) (TextBox-post tb))))
-
-
-;; Example for textbox-insert
-(define textbox5 (create-TextBox "World" "olleH"))
-
-(check-equal? 
-  (textbox-insert textbox5 "")
-  textbox5)
+(check-equal? (shift-char (create-TextBox "hel" "lo")) (create-TextBox "hell" "o"))
 
 
 ;; key-handler : TextBox String -> TextBox
 ;; Handles key inputs to modify the TextBox.
-(define (key-handler textbox key)
+;; - "left" moves the cursor one character left.
+;; - "right" moves the cursor one character right.
+;; - "backspace" removes the character before the cursor.
+;; - "delete" removes the character after the cursor.
+;; - All other single characters insert the character at the cursor.
+(define/contract (key-handler textbox key)
+  (-> TextBox? string? TextBox?)
   (cond
+    ;; Move the cursor left (pop from pre to post)
     [(string=? key "left")
      (textbox-left textbox)]
     
+    ;; Move the cursor right (pop from post to pre)
     [(string=? key "right")
      (textbox-right textbox)]
     
+    ;; Backspace (remove character before the cursor)
     [(string=? key "backspace")
      (textbox-backspace textbox)]
     
-    [(string=? key "delete")
+    ;; Delete (remove character after the cursor)
+    [(string=? key "\b")
      (textbox-delete textbox)]
-  
+    
+    ;; Insert any other single character
     [(= (string-length key) 1)
      (textbox-insert textbox key)]
     
-    [else
-     textbox]))  ; Ignore all other keys
+    ;; Ignore all other keys
+    [else textbox]))
 
 
-;; main : -> TextBox
-;; Runs the interactive program with a TextBox.
+;; Example key-handler tests
+(define textbox-example (create-TextBox "Hello" "World"))
+
+;; Move cursor left
+(check-equal? (key-handler textbox-example "left")
+              (textbox-left textbox-example))
+
+;; Move cursor right
+(check-equal? (key-handler textbox-example "right")
+              (textbox-right textbox-example))
+
+;; Backspace
+(check-equal? (key-handler textbox-example "backspace")
+              (textbox-backspace textbox-example))
+
+;; Delete
+(check-equal? (key-handler textbox-example "delete")
+              (textbox-delete textbox-example))
+
+;; Insert character "!"
+(check-equal? (key-handler textbox-example "!")
+              (textbox-insert textbox-example "!"))
+
+;; main : -> void
+;; Starts the interactive text editor with a blank TextBox.
 (define (main)
-  (big-bang (create-TextBox "" "")  ; Initial empty TextBox with empty strings
-    (on-key key-handler)              ; Handle key inputs
-    (to-draw render)))                ; Render the TextBox
-
+  (big-bang (create-TextBox "" "")  ;; Initial empty TextBox
+    (on-key key-handler)            ;; Handle key inputs
+    (to-draw render)))              ;; Render the TextBox
